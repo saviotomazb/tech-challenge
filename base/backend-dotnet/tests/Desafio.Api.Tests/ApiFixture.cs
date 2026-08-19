7277ba11e4da7202b67a988575ff1d80bdf1af1a
Savio Tomaz <saviotomaz_@hotmail.com>
2026-08-18 21:30:38 -0300

    feat: implementa API de beneficiários

--- base/backend-dotnet/tests/Desafio.Api.Tests/ApiFixture.cs
+++ base/backend-dotnet/tests/Desafio.Api.Tests/ApiFixture.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Testcontainers.PostgreSql;
+using Desafio.Api.Aplicacao;
 
 namespace Desafio.Api.Tests;
 
